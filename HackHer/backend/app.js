db8758d0b87996290a2cf4dd7d0c382272a89907
Masood Ahmed <mangimasood2000@gmail.com>
2023-01-21 17:13:00 -0500

    integrated everything

--- HackHer/backend/app.js
+++ HackHer/backend/app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const path=require('path')
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+// app.get('/', function (req, res) {
+//   res.sendFile("/Users/masoodahmed/Desktop/HackHer-hackathon/HackHer/Frontend/index.html");
+// })
+
+app.use(express.static(path.join(__dirname, "public"), {
+	index: "index.html"
+}))
 
 var server = app.listen(8081, function () {
   var host = server.address().address;
